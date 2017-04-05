1c53db15e7bd9fb1694d12d43483ccd146b49aa5
Geoff Bourne <itzgeoff@gmail.com>
2017-04-05 09:00:31 -0500

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs runit and provides runit_service definition'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 
 recipe 'ca_certificates', 'Installs and configures root ssl certificates'
 
